eb9d6187e3675a6b56063b0dc284420f3612ce9a
Wesley de Matos Souza <104766684+wesleydematos@users.noreply.github.com>
2024-01-19 13:30:46 -0300

    feat: create CoreNote model

---
+++ back/app/Models/CoreNote.ts
@@ -0,0 +1,25 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+
+export default class CoreNote extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public title: string
+
+  @column()
+  public description: string
+
+  @column()
+  public favorite: boolean
+
+  @column()
+  public color: string
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+}
