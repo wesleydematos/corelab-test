7571d2bb790c7b6e610be940fb04ace8b525ed04
Wesley de Matos Souza <104766684+wesleydematos@users.noreply.github.com>
2024-01-19 13:31:13 -0300

    feat: create CoreNote migration

---
+++ back/database/migrations/1705673934464_core_notes.ts
@@ -0,0 +1,25 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class CoreNotes extends BaseSchema {
+  protected tableName = 'core_notes'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('title')
+      table.string('description')
+      table.boolean('favorite')
+      table.string('color')
+
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
