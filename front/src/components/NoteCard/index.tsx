8dc2e6cfbcb2ed8f489321fd0f300e24f5dbc57e
Wesley de Matos Souza <104766684+wesleydematos@users.noreply.github.com>
2024-01-21 18:11:25 -0300

    feat: create update note logic

--- front/src/components/NoteCard/index.tsx
+++ front/src/components/NoteCard/index.tsx
@@ -1,5 +1,5 @@
-import { useState } from 'react'
-import { ICoreNoteResponse } from '../../types/CoreNote'
+import { FormEvent, useState } from 'react'
+import { ICoreNote, ICoreNoteResponse } from '../../types/CoreNote'
 import { FaCheck, FaTrashAlt } from 'react-icons/fa'
 import { VscChromeClose } from 'react-icons/vsc'
 import { SlPencil } from 'react-icons/sl'
@@ -18,8 +18,24 @@ interface INoteCardProps {
 
 function NoteCard({ note, setIsModified, isModified }: INoteCardProps) {
   const { id, color, description, title, favorite } = note
+  const colors = [
+    '#BAE2FF',
+    '#B9FFDD',
+    '#FFE8AC',
+    '#FFCAB9',
+    '#F99494',
+    '#9DD6FF',
+    '#ECA1FF',
+    '#DAFF8B',
+    '#FFA285',
+    '#CDCDCD',
+    '#979797',
+    '#A99A7C'
+  ]
 
   const [isEditing, setIsEditing] = useState(false)
+  const [isChangeColor, setIsChangeColor] = useState(false)
+  const [noteColor, setNoteColor] = useState(color)
   const [isDeleting, setIsDeleting] = useState(false)
   const [isFavotite, setIsFavorite] = useState(favorite)
   const [inputTitle, setInputTitle] = useState(title)
@@ -29,18 +45,47 @@ function NoteCard({ note, setIsModified, isModified }: INoteCardProps) {
     try {
       await api.delete(`/${id}`)
       setIsModified(!isModified)
-      toast.success('Note has been deleted')
+      toast.success('Nota deletada!')
     } catch (error) {
       console.log(error)
-      alert('não deletou')
+      toast.error('Não foi possível deletar!')
+    }
+  }
+
+  async function handleSubmit(event: FormEvent) {
+    event.preventDefault()
+
+    const data: ICoreNote  = {
+      color: noteColor,
+      description: content,
+      title: inputTitle,
+      favorite: isFavotite
+    }
+
+    try {
+      await api.put(`${id}`, data)
+      setIsEditing(false)
+      setIsModified(!isModified)
+      toast.success('Nota editada com sucesso!')
+    } catch (error) {
+      console.log(error)
+      setIsEditing(false)
+      setIsChangeColor(false)
+      setNoteColor(color)
+      setIsFavorite(favorite)
+      setInputTitle(title)
+      setContent(description)
+      toast.error('Não foi possível editar a nota!')
     }
   }
 
   return (
-    <li style={{backgroundColor: color}}>
-      <form>
+    <li style={{backgroundColor: noteColor}}>
+      <form onSubmit={handleSubmit}>
         <div className='title'>
           <input
+            onFocus={()=> setIsEditing(true)}
+            style={{backgroundColor: noteColor}}
             type='text'
             placeholder='Título'
             value={inputTitle}
@@ -49,11 +94,18 @@ function NoteCard({ note, setIsModified, isModified }: INoteCardProps) {
           <img 
             src={isFavotite ? yellowStar : star} 
             alt='star' 
-            onClick={()=>setIsFavorite(!isFavotite)}
+            onClick={()=>{
+              if(isFavotite == favorite){ 
+                setIsEditing(true)
+              } 
+              setIsFavorite(!isFavotite)
+            }}
           />
         </div>
 
         <textarea
+          onFocus={()=> setIsEditing(true)}
+          style={{backgroundColor: noteColor}}
           placeholder='Edite a nota...'
           value={content}
           onChange={(e) => setContent(e.target.value)}
@@ -61,9 +113,31 @@ function NoteCard({ note, setIsModified, isModified }: INoteCardProps) {
 
         <div className='editing'>
           <div>
-            { isEditing ? <FaCheck /> : <SlPencil />}
-            <div style={{backgroundColor: isEditing ? '#ffe8ac' : '#ffffff'}}>
-              <img src={ink} alt='ink' />
+            { 
+              isEditing ? 
+              <>
+                <button type='submit'><FaCheck /></button>
+                <VscChromeClose 
+                  onClick={()=> {
+                    setIsEditing(false)
+                    setIsChangeColor(false)
+                    setNoteColor(color)
+                    setIsFavorite(favorite)
+                    setInputTitle(title)
+                    setContent(description)
+                  }}
+                />
+              </>
+              : 
+              <SlPencil onClick={()=>setIsEditing(true)}/>
+            }
+            <div 
+              onClick={()=> {
+                setIsChangeColor(!isChangeColor) 
+                setIsEditing(true)
+              }}
+            >
+              <img src={ink} alt='ink'/>
             </div>
           </div>
           {isDeleting ? 
@@ -81,6 +155,18 @@ function NoteCard({ note, setIsModified, isModified }: INoteCardProps) {
           }
         </div>
       </form>
+      {
+        isChangeColor && 
+        <div className='changeColor'>
+          {colors.map((item)=> {
+            return <div 
+              key={item} 
+              style={{backgroundColor: item}}
+              onClick={()=> {setNoteColor(item); setIsChangeColor(false)}}
+            />
+          })}
+        </div>
+      }
     </li>
   )
 }
